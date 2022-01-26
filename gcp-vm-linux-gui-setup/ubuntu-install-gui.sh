7c213568fd2ea2e1402d5a20e51c6adb0dc16bcb
Kunal Suri <kunalsuri.vit@gmail.com>
2022-01-26 14:48:40 +0100

    Updated the script to display the comments
    Updated the script to display the comments

--- gcp-vm-linux-gui-setup/ubuntu-install-gui.sh
+++ gcp-vm-linux-gui-setup/ubuntu-install-gui.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
-
-echo '==== This script will install the GUI on your Ubuntu Server. ===='
-echo '==== It will also create a user account for enabling Remote Desktop Connections ===='
-echo '==== Username: ' $1
-echo '==== Password: ' $2
-
+echo ''
+echo '==== This script will install and enable the GUI on your Ubuntu Server.'
+echo '==== This script will also create a user account to allow Remote Desktop Connections.'
+echo ''
+echo '==== The username entered is: ' $1
+echo '==== The password entered is: ' $2
 echo ''
 
-echo '==== Upgrade system ===='
+echo '==== To allow the system to be updated/upgraded.'
+echo ''
 sudo apt update
 sudo apt upgrade -y
+echo ''
 
-echo '==== Install packages ===='
-sudo apt install -y gnome-shell
-sudo apt install -y ubuntu-gnome-desktop
-sudo apt install -y autocutsel
-sudo apt install -y xrdp
+echo '==== The installation of the following packages will take place'
+echo ''
+sudo apt install -y gnome-shell	ubuntu-gnome-desktop autocutsel xrdp
+echo ''
+echo '==== Enabling the xrdp...'
+echo ''
 sudo systemctl enable xrdp
-sudo apt install -y gnome-core
-sudo apt install -y gnome-panel
-sudo apt install -y synaptic
+echo ''
+sudo apt install -y gnome-core gnome-panel synaptic
 
 touch ~/.Xresources
 
 sudo adduser  --disabled-password --gecos "GUI User" $1
+
 echo "$1:$2" | sudo chpasswd
+echo ''
+echo '==== Istallation completed. Thanks!'
