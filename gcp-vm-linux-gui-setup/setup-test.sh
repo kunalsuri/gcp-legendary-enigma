69919a48aacb0471192112c87f6adf0dbb5a8e1e
Kunal Suri <kunalsuri.vit@gmail.com>
2022-01-26 14:34:43 +0100

    Update setup-test.sh

--- gcp-vm-linux-gui-setup/setup-test.sh
+++ gcp-vm-linux-gui-setup/setup-test.sh
@@ -3,9 +3,6 @@
 echo '==== This is a test script.'
 echo ''
 echo '==== This script will check if there is anything to upgrade in your system.'
-echo ''
 sudo apt upgrade -y
-echo ''
-echo ''
 echo '==== The test script has run successfully. Thanks!'
 echo ''
