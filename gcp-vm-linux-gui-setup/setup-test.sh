03298f31d8c3c4a4171e48be1a78ea6134e5ffe4
Kunal Suri <kunalsuri.vit@gmail.com>
2022-01-26 14:32:57 +0100

    Included a space

--- gcp-vm-linux-gui-setup/setup-test.sh
+++ gcp-vm-linux-gui-setup/setup-test.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
 echo '==== This is a test script.'
-
 echo ''
-
 echo '==== This script will check if there is anything to upgrade in your system.'
-
+echo ''
 sudo apt upgrade -y
-
+echo ''
+echo ''
 echo '==== The test script has run successfully. Thanks!'
-
 echo ''
